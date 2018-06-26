1220b872dfef37fd92e8743fa0c321b7487ef547
Miracle <120007700@qq.com>
2018-06-26 16:32:51 +0800

    注册与登录

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,5 +1,5 @@
 <?php
 function route_class()
 {
-    return str_replace('.', '-', \Illuminate\Support\Facades\Route::currentRouteName());
+    return str_replace('.', '-', Route::currentRouteName());
 }
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,6 @@
 */
 
 Route::get('/', 'PagesController@root')->name('root');
+Auth::routes();
+
+//Route::get('/home', 'HomeController@index')->name('home');
