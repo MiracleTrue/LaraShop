4862c4776c1fd58984f9e2b81acc0abf46575203
Miracle <120007700@qq.com>
2018-06-27 16:30:32 +0800

    修改和删除收货地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -10,6 +10,7 @@ class UserAddressesController extends Controller
 {
     public function index(Request $request)
     {
+
         return view('user_addresses.index', [
             'addresses' => $request->user()->addresses,
         ]);
@@ -36,5 +37,55 @@ class UserAddressesController extends Controller
         ]));
 
         return redirect()->route('user_addresses.index');
+    }
+
+    /**
+     * @param UserAddress $user_address
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function edit(UserAddress $user_address)
+    {
+        $this->authorize('own', $user_address);
+
+        return view('user_addresses.create_and_edit', ['address' => $user_address]);
+    }
+
+    /**
+     * @param UserAddress $user_address
+     * @param UserAddressRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function update(UserAddress $user_address, UserAddressRequest $request)
+    {
+        $this->authorize('own', $user_address);
+
+        $user_address->update($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        return redirect()->route('user_addresses.index');
+    }
+
+    /**
+     * @param UserAddress $user_address
+     * @return array
+     * @throws \Exception
+     */
+    public function destroy(UserAddress $user_address)
+    {
+        $this->authorize('own', $user_address);
+
+        $user_address->delete();
+
+        return [];
+//        return redirect()->route('user_addresses.index');
     }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\UserAddress;
+use App\Policies\UserAddressPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -9,16 +11,14 @@ class AuthServiceProvider extends ServiceProvider
 {
     /**
      * The policy mappings for the application.
-     *
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+        UserAddress::class => UserAddressPolicy::class,
     ];
 
     /**
      * Register any authentication / authorization services.
-     *
      * @return void
      */
     public function boot()
