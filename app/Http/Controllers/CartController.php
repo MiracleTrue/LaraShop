4ceecac8ca42c47bdcb14b02086884ce5f4e0752
Miracle <120007700@qq.com>
2018-06-29 14:08:14 +0800

    购物车页面

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,9 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\AddCartRequest;
 use App\Models\CartItem;
+use App\Models\ProductSku;
+use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
+
+    public function index(Request $request)
+    {
+        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+
+        return view('cart.index', ['cartItems' => $cartItems]);
+    }
+
 
     public function add(AddCartRequest $request)
     {
@@ -30,6 +40,13 @@ class CartController extends Controller
             $cart->productSku()->associate($skuId);
             $cart->save();
         }
+        return [];
+    }
+
+    public function remove(ProductSku $sku, Request $request)
+    {
+        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
+
         return [];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,9 @@ Route::group(['middleware' => 'auth'], function () {
         Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
 
         //购物车
+        Route::get('cart', 'CartController@index')->name('cart.index');
         Route::post('cart', 'CartController@add')->name('cart.add');
+        Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
     });
 });
 
