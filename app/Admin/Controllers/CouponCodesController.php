953c56e5890ea92524b867d03271b62547af4fe2
Miracle <120007700@qq.com>
2018-07-05 10:14:02 +0800

    管理后台 - 新增、修改、删除优惠券

--- app/Admin/Controllers/CouponCodesController.php
+++ app/Admin/Controllers/CouponCodesController.php
@@ -17,7 +17,6 @@ class CouponCodesController extends Controller
 
     /**
      * Index interface.
-     *
      * @return Content
      */
     public function index()
@@ -30,40 +29,80 @@ class CouponCodesController extends Controller
 
     /**
      * Edit interface.
-     *
      * @param $id
      * @return Content
      */
     public function edit($id)
     {
         return Admin::content(function (Content $content) use ($id) {
-
-            $content->header('header');
-            $content->description('description');
-
+            $content->header('编辑优惠券');
             $content->body($this->form()->edit($id));
         });
     }
 
-    /**
-     * Create interface.
-     *
-     * @return Content
-     */
     public function create()
     {
         return Admin::content(function (Content $content) {
-
-            $content->header('header');
-            $content->description('description');
-
+            $content->header('新增优惠券');
             $content->body($this->form());
+        });
+    }
+
+    protected function form()
+    {
+        return Admin::form(CouponCode::class, function (Form $form) {
+            $form->display('id', 'ID');
+            $form->text('name', '名称')->rules('required');
+            $form->text('code', '优惠码')->rules(function ($form) {
+                // 如果 $form->model()->id 不为空，代表是编辑操作
+                if ($id = $form->model()->id)
+                {
+                    return 'nullable|unique:coupon_codes,id,' . $id;
+                } else
+                {
+                    return 'nullable|unique:coupon_codes';
+                }
+            });
+            $form->radio('type', '类型')->options(CouponCode::$typeMap)->rules('required');
+            $form->text('value', '折扣')->rules(function ($form) {
+                if ($form->type === CouponCode::TYPE_PERCENT)
+                {
+                    // 如果选择了百分比折扣类型，那么折扣范围只能是 1 ~ 99
+                    return 'required|numeric|between:1,99';
+                } else
+                {
+                    // 否则只要大等于 0.01 即可
+                    return 'required|numeric|min:0.01';
+                }
+            });
+
+            $form->text('total', '总量')->rules('required|numeric|min:0');
+            $form->text('min_amount', '最低金额')->rules(function ($form) {
+                // 如果是固定金额，则最低订单金额必须要比优惠金额高 1 元
+                if (request('type') === CouponCode::TYPE_FIXED)
+                {
+                    return 'required|numeric|min:' . request('value');
+                } else
+                {
+                    return 'nullable';
+                }
+
+            });
+            $form->datetime('not_before', '开始时间');
+            $form->datetime('not_after', '结束时间');
+            $form->radio('enabled', '启用')->options(['1' => '是', '0' => '否']);
+
+            $form->saving(function (Form $form) {
+                if (!$form->code)
+                {
+                    $form->code = CouponCode::findAvailableCode();
+                }
+            });
         });
     }
 
     /**
      * Make a grid builder.
-     *
      * @return Grid
      */
     protected function grid()
@@ -85,19 +124,4 @@ class CouponCodesController extends Controller
         });
     }
 
-    /**
-     * Make a form builder.
-     *
-     * @return Form
-     */
-    protected function form()
-    {
-        return Admin::form(CouponCode::class, function (Form $form) {
-
-            $form->display('id', 'ID');
-
-            $form->display('created_at', 'Created At');
-            $form->display('updated_at', 'Updated At');
-        });
-    }
 }
